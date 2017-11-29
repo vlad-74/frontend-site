3927fe9cd238dd6e87c7c4704f36d2459e7fcd3b
Vladimi Kurguzov <kurguzov74@gmail.com>
2017-11-29 03:03:09 +0300

    first commit

--- site.bat
+++ site.bat
@@ -1,3 +1,3 @@
-https://www.youtube.com/watch?v=mhNusXCJBHE
-http://isizov.ru/kak-dobavit-domen-k-github-pages/
-https://www.reg.ru/
+start "" https://www.youtube.com/watch?v=mhNusXCJBHE
+start "" http://isizov.ru/kak-dobavit-domen-k-github-pages/
+start "" https://www.reg.ru/
